f8cea7ab73f3a667cd87da07fe3ebcb753c04cb3
Kim Sanghyeon <notingcode@gmail.com>
2023-09-19 11:32:41 +0900

    mdm to openpose modification code complete

--- mdm_to_openpose.py
+++ mdm_to_openpose.py
@@ -1,18 +1,16 @@
 import os
 import numpy as np
 import open3d as o3d
-from joint_format import (
-    MDM_JOINT_MAP, MDM_JOINT_PAIRS,
-    OPENPOSE_JOINT_MAP, OPENPOSE_JOINT_PAIRS
-)
+from joint_format import *
 from pathlib import Path
-import colorutils
+from sklearn.preprocessing import normalize
 
 
 class mdmCustumVisualize():
     def __init__(self, npy_filepath: Path | str) -> None:
-        if (type(npy_filepath) is str):
+        if npy_filepath is not Path:
             npy_filepath = Path(npy_filepath)
+        
         if not npy_filepath.exists():
             raise FileNotFoundError()
 
@@ -34,6 +32,7 @@ class mdmCustumVisualize():
             pass
         elif keypoint_type == 'coco-18':
             pass
+        
     def convert_mdm2openpose(self):
         if self.mdm_motion.shape[3] == 22:
             def create_new_neck():
@@ -45,31 +44,91 @@ class mdmCustumVisualize():
                 right_shoulder = self.mdm_motion[:,MDM_JOINT_MAP['RightShoulder'],:,:]
                 spine2 = self.mdm_motion[:,MDM_JOINT_MAP['Spine2'],:,:]
                 
-                self.mdm_motion[:,MDM_JOINT_MAP['Spine2'],:,:] = (left_shoulder+right_shoulder)*0.45 + spine2*0.1
+                new_neck = (left_shoulder+right_shoulder)*0.45 + spine2*0.1
+                
+                return new_neck
                 
             def create_eyes_ears():
                 """
                 
                 """
-                pass
-            
                 
-            def adjust_uplegs2hips():
+                def norm_of_3D_plane(v1, v2, v3):
+                    """_summary_
+
+                    Args:
+                        v1 (_type_): _description_
+                        v2 (_type_): _description_
+                        v3 (_type_): _description_
+
+                    Returns:
+                        _type_: _description_
+                    """
+                    
+                    a = v1 - v2
+                    b = v2 - v3
+                    
+                    normal_vector = np.cross(a,b,axis=1)
+                    
+                    for frame in range(normal_vector.shape[-1]):
+                        normal_vector[:,:,frame] = normalize(normal_vector[:,:,frame])
+                        
+                    return normal_vector
+                
+                head = self.mdm_motion[:,MDM_JOINT_MAP['Head'],:,:]
+                neck = self.mdm_motion[:,MDM_JOINT_MAP['Neck'],:,:]
+                spine2 = self.mdm_motion[:,MDM_JOINT_MAP['Spine2'],:,:]
+
+                normal_vector = norm_of_3D_plane(head, neck, spine2)
+
+                normal_vector_eyes = np.divide(normal_vector,20)
+                normal_vector_ears = np.divide(normal_vector,13)
+
+                left_eye = head - normal_vector_eyes
+                right_eye = head + normal_vector_eyes
+
+                left_ear = head - normal_vector_ears
+                right_ear = head + normal_vector_ears
+                
+                norm_to_mv_eyes_ears = norm_of_3D_plane(neck, left_eye, right_eye)
+                norm_to_mv_eyes = np.divide(norm_to_mv_eyes_ears, 22)
+                norm_to_mv_ears = np.divide(norm_to_mv_eyes_ears, 20)
+                
+                left_eye = left_eye - norm_to_mv_eyes
+                right_eye = right_eye - norm_to_mv_eyes
+                
+                left_ear = left_ear - norm_to_mv_ears
+                right_ear = right_ear - norm_to_mv_ears
+                
+                norm_to_mv_eyes_ears_down = norm_of_3D_plane(head, left_eye, right_eye)
+                # norm_to_mv_eyes_down = np.divide(norm_to_mv_eyes_ears_down, 30)
+                norm_to_mv_ears_down = np.divide(norm_to_mv_eyes_ears_down, 13)
+                
+                # left_eye = left_eye - norm_to_mv_eyes_down
+                # right_eye = right_eye - norm_to_mv_eyes_down
+                
+                left_ear = left_ear - norm_to_mv_ears_down
+                right_ear = right_ear - norm_to_mv_ears_down
+                
+                return (right_eye, left_eye, right_ear, left_ear)            
+                
+            def create_rlHips():                
                 """
                 
                 """
+                
                 left_upleg = self.mdm_motion[:,MDM_JOINT_MAP['LeftUpLeg'],:,:]
                 right_upleg = self.mdm_motion[:,MDM_JOINT_MAP['RightUpLeg'],:,:]
-                
+
+                left_hip = (left_upleg*1.1+right_upleg*-0.1)                
                 right_hip = (left_upleg*-0.1+right_upleg*1.1)
-                left_hip = (left_upleg*1.1+right_upleg*-0.1)
                 
-                self.mdm_motion[:,MDM_JOINT_MAP['LeftUpLeg'],:,:] = left_hip
-                self.mdm_motion[:,MDM_JOINT_MAP['RightUpLeg'],:,:] = right_hip
+                return (right_hip, left_hip)
                 
-            create_new_neck()
-            create_eyes_ears()
-            adjust_uplegs2hips()
+            new_neck = create_new_neck()
+            (right_eye, left_eye, right_ear, left_ear) = create_eyes_ears()
+            (right_hip, left_hip) = create_rlHips()
+            
         elif self.mdm_motion.shape[3] == 18:
             print("Already in OpenPose format")
         else:
