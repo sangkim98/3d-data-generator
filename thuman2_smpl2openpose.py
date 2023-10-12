a49e48db6057e966d707a9a17f4389bf30350f4c
notingcode <notingcode@gmail.com>
2023-10-12 16:11:15 +0900

    splitting module functionalities for future development

--- thuman2_smpl2openpose.py
+++ thuman2_smpl2openpose.py
@@ -1,108 +1,14 @@
 import os
 import argparse
+import json
 from pathlib import Path
 import numpy as np
 import open3d as o3d
 import mitsuba as mi
-from sklearn.preprocessing import normalize
-from mdm_to_openpose import mdm2openpose
+from mdm2openpose import mdm2openpose
 from joint_format import *
 
 TEST_NAME = 'test2'
-
-def render_mesh(mesh, mesh_center, exr_path: str):
-    scene = mi.load_dict({
-        'type': 'scene',
-        'integrator': {
-            'type': 'path'
-        },
-        'light': {
-            # NOTE: For better results comment out the constant emitter above
-            # and uncomment out the lines below changing the filename to an HDRI
-            # envmap you have.
-            'type': 'envmap',
-            'filename': exr_path,
-            'scale': 1.0,
-        },
-        'sensor': {
-            'type': 'batch',
-            'sensor1': {
-                'type': 'perspective',
-                'fov': 60,
-                'to_world': mi.ScalarTransform4f.look_at(origin=[0,0.6+mesh_center[1],1.5],
-                                                         target=mesh_center,
-                                                         up=[0,1,0]
-                                                         )
-            },
-            'sensor2': {
-                'type': 'perspective',
-                'fov': 60,
-                'to_world': mi.ScalarTransform4f.look_at(origin=[0,0.6+mesh_center[1],-1.5],
-                                                         target=mesh_center,
-                                                         up=[0,1,0]
-                                                         )
-            },
-            'sensor3': {
-                'type': 'perspective',
-                'fov': 60,
-                'to_world': mi.ScalarTransform4f.look_at(origin=[1.5,0.6+mesh_center[1],0],
-                                                         target=mesh_center,
-                                                         up=[0,1,0]
-                                                         )
-            },
-            'sensor4': {
-                'type': 'perspective',
-                'fov': 60,
-                'to_world': mi.ScalarTransform4f.look_at(origin=[-1.5,0.6+mesh_center[1],0],
-                                                         target=mesh_center,
-                                                         up=[0,1,0]
-                                                         )
-            },
-            'thefilm': {
-                'type': 'hdrfilm',
-                'width': 1024*4,
-                'height': 1024,
-            },
-            'thesampler': {
-                'type': 'multijitter',
-                'sample_count': 64,
-            },
-        },
-        'themesh': mesh,
-    })
-
-    img = mi.render(scene, spp=256)
-    return img
-
-def create_hdri(model_path, exr_path):
-    mi.set_variant('cuda_ad_rgb')
-    
-    obj_path = Path(model_path).glob('*.obj')
-    texture_path = Path(model_path).glob('*.jpeg')
-    
-    for path in obj_path:
-        obj_path = path
-    for path in texture_path:
-        texture_path = path
-    
-    mesh = o3d.io.read_triangle_mesh(obj_path.as_posix(), True)
-    mesh.textures = [o3d.io.read_image(texture_path.as_posix())]
-
-    scale_center = mesh.get_center()
-
-    mesh.scale(1.3, center=scale_center)    
-    
-    tensor_mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
-    mesh_center = tensor_mesh.get_axis_aligned_bounding_box().get_center()
-    
-    mi_mesh = tensor_mesh.to_mitsuba('thuman')
-    img = render_mesh(mi_mesh, mesh_center.numpy(), exr_path)
-    png_out_model = mi.Bitmap(img).convert(mi.Bitmap.PixelFormat.RGB, mi.Struct.Type.UInt16, True)
-    
-    exr = mi.Bitmap(exr_path)
-    png_out_exr = exr.convert(mi.Bitmap.PixelFormat.RGB, mi.Struct.Type.UInt16, True)
-    
-    return png_out_model, png_out_exr, mesh_center.numpy(), scale_center
 
 def remove_wrists_from_smpl(path):
     smpl_path = Path(path).glob('*.ply')
@@ -263,51 +169,51 @@ def main():
                 
         return openpose_data_edited
 
-    # new_smpl = convert_mdm2openpose(new_smpl_np)
+    new_smpl = convert_mdm2openpose(new_smpl_np)
 
-    # point_colors = []
-    # joint_pair_idxs = []
-    # for color_rgb in OPENPOSE_18JOINT_COLOR.values():
-    #     point_colors.append(color_rgb)
-    # for joint_pair in OPENPOSE_18JOINT_PAIRS:
-    #     joint_pair_idxs.append([OPENPOSE_18JOINT_MAP[joint_pair[0]],OPENPOSE_18JOINT_MAP[joint_pair[1]]])
+    point_colors = []
+    joint_pair_idxs = []
+    for color_rgb in OPENPOSE_18JOINT_COLOR.values():
+        point_colors.append(color_rgb)
+    for joint_pair in OPENPOSE_18JOINT_PAIRS:
+        joint_pair_idxs.append([OPENPOSE_18JOINT_MAP[joint_pair[0]],OPENPOSE_18JOINT_MAP[joint_pair[1]]])
         
-    # point_colors = np.asarray(point_colors)
-    # point_colors = np.divide(point_colors,255)
-    # joint_pair_idxs = np.asarray(joint_pair_idxs)
-    # joint_pair_colors = np.asarray(OPENPOSE_18JOINT_PAIRS_COLOR)
-    # joint_pair_colors = np.divide(joint_pair_colors,255)
+    point_colors = np.asarray(point_colors)
+    point_colors = np.divide(point_colors,255)
+    joint_pair_idxs = np.asarray(joint_pair_idxs)
+    joint_pair_colors = np.asarray(OPENPOSE_18JOINT_PAIRS_COLOR)
+    joint_pair_colors = np.divide(joint_pair_colors,255)
     
-    # pcd = o3d.geometry.PointCloud()
-    # lineSet = o3d.geometry.LineSet()
+    pcd = o3d.geometry.PointCloud()
+    lineSet = o3d.geometry.LineSet()
     
-    # mat_line = o3d.visualization.rendering.MaterialRecord()
-    # mat_point = o3d.visualization.rendering.MaterialRecord()
+    mat_line = o3d.visualization.rendering.MaterialRecord()
+    mat_point = o3d.visualization.rendering.MaterialRecord()
 
-    # mat_line.shader = "unlitLine"
-    # mat_line.line_width = 15
-    # mat_point.point_size = 15
+    mat_line.shader = "unlitLine"
+    mat_line.line_width = 15
+    mat_point.point_size = 15
     
-    # renderer = o3d.visualization.rendering.OffscreenRenderer(1024,1024)
-    # renderer.scene.set_background(np.array([0,0,0,0]))
-    # pcd.points = o3d.utility.Vector3dVector(new_smpl)
-    # pcd.colors = o3d.utility.Vector3dVector(point_colors)
-    # pcd.scale(1.3, center=scale_center)
-    # lineSet.points = pcd.points
-    # lineSet.lines = o3d.utility.Vector2iVector(joint_pair_idxs)
-    # lineSet.colors = o3d.utility.Vector3dVector(joint_pair_colors)
-    # renderer.scene.add_geometry("pcd",pcd,mat_point)
-    # renderer.scene.add_geometry("lineset",lineSet,mat_line)
+    renderer = o3d.visualization.rendering.OffscreenRenderer(1024,1024)
+    renderer.scene.set_background(np.array([0,0,0,0]))
+    pcd.points = o3d.utility.Vector3dVector(new_smpl)
+    pcd.colors = o3d.utility.Vector3dVector(point_colors)
+    pcd.scale(1.3, center=scale_center)
+    lineSet.points = pcd.points
+    lineSet.lines = o3d.utility.Vector2iVector(joint_pair_idxs)
+    lineSet.colors = o3d.utility.Vector3dVector(joint_pair_colors)
+    renderer.scene.add_geometry("pcd",pcd,mat_point)
+    renderer.scene.add_geometry("lineset",lineSet,mat_line)
     
-    # camera_position = {'front': [0,0.6+mesh_center[1],1.5], 'back': [0,0.6+mesh_center[1],-1.5], 'left': [1.5,0.6+mesh_center[1],0], 'right': [-1.5,0.6+mesh_center[1],0]}
+    camera_position = {'front': [0,0.6+mesh_center[1],1.5], 'back': [0,0.6+mesh_center[1],-1.5], 'left': [1.5,0.6+mesh_center[1],0], 'right': [-1.5,0.6+mesh_center[1],0]}
     
-    # for angle, value in camera_position.items():
-    #     renderer.setup_camera(60,
-    #                             mesh_center,
-    #                             np.array(value),
-    #                             np.array([0,1,0])
-    #                             )
-    #     img_o3d = renderer.render_to_image()
-    #     o3d.io.write_image(os.path.join(args.destinationPath, f"{angle}.png"), img_o3d)
+    for angle, value in camera_position.items():
+        renderer.setup_camera(60,
+                                mesh_center,
+                                np.array(value),
+                                np.array([0,1,0])
+                                )
+        img_o3d = renderer.render_to_image()
+        o3d.io.write_image(os.path.join(args.destinationPath, f"{angle}.png"), img_o3d)
     
 main()
