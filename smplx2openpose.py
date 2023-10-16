78640c86b99d0a5af4d0a7a5ea648476518a650f
Kim Sanghyeon <notingcode@gmail.com>
2023-10-16 18:21:36 +0900

    trying to create smpl mesh from parameters

--- smplx2openpose.py
+++ smplx2openpose.py
@@ -2,15 +2,19 @@ import os
 import torch
 import smplx
 import numpy as np
-from joint_format import SMPLX_JOINT_NAMES
+from convert2openpose import convert2openpose
+from joint_format import SMPLX2OPENPOSE_IDX
+import open3d as o3d
 
-JOINTS_IDX = [22,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54]
+JOINTS_IDX = [0,3,6,9,10,11,13,14,15,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54]
 
-class smplx2openpose():
-   def __init__(self, models_path: str, smplx_param_path: str, gender='neutral') -> None:
+class smplx2openpose(convert2openpose):
+   def __init__(self, models_path: str, smplx_param_path: str, gender='male') -> None:
+      super().__init__()
+      
       def create_smplx_model(models_path: str, smplx_param_path: str, gender: str):
          model_name = f"SMPLX_{gender.upper()}.npz"
-         smplx_model_template = smplx.SMPLX(os.path.join(models_path,model_name))
+         smplx_model_template = smplx.SMPLX(models_path, gender='male')
          
          thuman_joints = np.load(smplx_param_path, allow_pickle=True)
          
@@ -19,28 +23,34 @@ class smplx2openpose():
          global_orientation = smplx.utils.Tensor(torch.as_tensor(thuman_joints['global_orient']))
          expression = smplx.utils.Tensor(torch.as_tensor(thuman_joints['expression']))
          jaw_pose = smplx.utils.Tensor(torch.as_tensor(thuman_joints['jaw_pose']))
+         # lh_pose = smplx.utils.Tensor(torch.as_tensor(thuman_joints['left_hand_pose']))
+         # rh_pose = smplx.utils.Tensor(torch.as_tensor(thuman_joints['right_hand_pose']))
          leye_pose = smplx.utils.Tensor(torch.as_tensor(thuman_joints['leye_pose']))
          reye_pose = smplx.utils.Tensor(torch.as_tensor(thuman_joints['reye_pose']))
+         transl = smplx.utils.Tensor(torch.as_tensor(np.asarray(thuman_joints['translation']).reshape(1,3)))
+         scale = thuman_joints['scale'][0]
 
          smplx_model = smplx_model_template.forward(betas=betas,
                                                     global_orient=global_orientation,
                                                     body_pose=pose,
+                                                    transl=transl,
                                                     expression=expression,
                                                     jaw_pose=jaw_pose,
+                                                   #  left_hand_pose=lh_pose,
+                                                   #  right_hand_pose=rh_pose,
                                                     leye_pose=leye_pose,
-                                                    reye_pose=reye_pose
+                                                    reye_pose=reye_pose,
                                                    )
 
-         return smplx_model
+         return smplx_model, scale
 
-      joints = create_smplx_model(models_path, smplx_param_path, gender).joints.detach().numpy()
+      model, scale = create_smplx_model(models_path, smplx_param_path, gender)
 
-      joints_reshaped = joints.reshape(127,3)
+      print(model.vertices.shape)
+
+      joints_reshaped = model.joints.detach().numpy().reshape(127,3)
       
-      self.joints = np.delete(joints_reshaped[:60,:], JOINTS_IDX, axis=0)
+      joints = np.delete(joints_reshaped[:60,:], JOINTS_IDX, axis=0)
       
-def main():
-   test = smplx2openpose('/home/notingcode/Projects/3d_visualize/models/smplx', '/home/notingcode/Desktop/Thuman2_SMPLX/0000/smplx_param.pkl')
-   print(test.joints.shape)
-   
-main()
+      self.joints = joints[SMPLX2OPENPOSE_IDX]
+      self.scale = scale
--- thuman2_smpl2openpose.py
+++ thuman2_smpl2openpose.py
@@ -5,10 +5,11 @@ from pathlib import Path
 import numpy as np
 import open3d as o3d
 import mitsuba as mi
-from mdm2openpose import mdm2openpose
 from joint_format import *
+import multiview_rendering
+from smplx2openpose import smplx2openpose
 
-TEST_NAME = 'test2'
+TEST_NAME = 'test4'
 
 def remove_wrists_from_smpl(path):
     smpl_path = Path(path).glob('*.ply')
@@ -49,128 +50,18 @@ def main():
     if not os.path.exists(args.destinationPath):
         os.makedirs(args.destinationPath)
 
-    png_out_model, png_out_exr, mesh_center, scale_center = create_hdri(smpl_path, exr_path)
+    openpose_params = smplx2openpose('/home/notingcode/Projects/3d_visualize/models/smplx',
+                                     '/home/notingcode/Projects/3d_visualize/Thuman2_data/Thuman2_SMPLX/0026/smplx_param.pkl'
+                                     )
+    
+    openpose_joints = openpose_params.joints
+    scale = openpose_params.scale
+
+    png_out_model, png_out_exr, mesh_center, scale_center = multiview_rendering.create_hdri(smpl_path, exr_path)
 
     png_out_model.write(os.path.join(dest_path, f"{TEST_NAME}.png"))
     png_out_exr.write(os.path.join(dest_path, f"{TEST_NAME}_hdri.png"))
-
-    new_smpl_np = remove_wrists_from_smpl(smpl_path)
-
-    def convert_mdm2openpose(new_smpl_np):
-        def norm_of_3D_plane(v1, v2, v3):
-            """_summary_
-
-            Args:
-                v1 (_type_): _description_
-                v2 (_type_): _description_
-                v3 (_type_): _description_
-
-            Returns:
-                _type_: _description_
-            """
-            
-            a = v1 - v2
-            b = v2 - v3
-            
-            normal_vector = np.cross(a,b)
-            
-            normal_vector = normal_vector / np.linalg.norm(normal_vector)
-                
-            return normal_vector
-        
-        def create_new_neck():
-            """
-            
-            """
-        
-            left_shoulder = new_smpl_np[MDM_22JOINT_MAP['LeftShoulder'],:]
-            right_shoulder = new_smpl_np[MDM_22JOINT_MAP['RightShoulder'],:]
-            spine2 = new_smpl_np[MDM_22JOINT_MAP['Spine2'],:]
-            
-            new_neck = (left_shoulder+right_shoulder)*0.45 + spine2*0.1
-            
-            return new_neck
-            
-        def create_eyes_ears():
-            """
-            
-            """
-            
-            head = new_smpl_np[MDM_22JOINT_MAP['Head'],:]
-            neck = new_smpl_np[MDM_22JOINT_MAP['Neck'],:]
-            spine2 = new_smpl_np[MDM_22JOINT_MAP['Spine2'],:]
-
-            normal_vector = norm_of_3D_plane(head, neck, spine2)
-
-            normal_vector_eyes = np.divide(normal_vector,25)
-            normal_vector_ears = np.divide(normal_vector,20)
-
-            left_eye = head - normal_vector_eyes
-            right_eye = head + normal_vector_eyes
-
-            left_ear = head - normal_vector_ears
-            right_ear = head + normal_vector_ears
-            
-            norm_to_mv_eyes_ears = norm_of_3D_plane(neck, left_eye, right_eye)
-            norm_to_mv_eyes = np.divide(norm_to_mv_eyes_ears, 25)
-            norm_to_mv_ears = np.divide(norm_to_mv_eyes_ears, 22)
-            
-            left_eye = left_eye - norm_to_mv_eyes
-            right_eye = right_eye - norm_to_mv_eyes
-            
-            left_ear = left_ear - norm_to_mv_ears
-            right_ear = right_ear - norm_to_mv_ears
-            
-            norm_to_mv_eyes_ears_down = norm_of_3D_plane(head, left_eye, right_eye)
-            norm_to_mv_ears_down = np.divide(norm_to_mv_eyes_ears_down, 18)
-            
-            left_ear = left_ear - norm_to_mv_ears_down
-            right_ear = right_ear - norm_to_mv_ears_down
-            
-            return (right_eye, left_eye, right_ear, left_ear)            
-            
-        def create_rlHips():                
-            """
-            
-            """
-            
-            left_upleg = new_smpl_np[MDM_22JOINT_MAP['LeftUpLeg'],:]
-            right_upleg = new_smpl_np[MDM_22JOINT_MAP['RightUpLeg'],:]
-
-            left_hip = (left_upleg*1.1+right_upleg*-0.1)                
-            right_hip = (left_upleg*-0.1+right_upleg*1.1)
-            
-            return (right_hip, left_hip)
-            
-        new_neck = create_new_neck()
-        (right_eye, left_eye, right_ear, left_ear) = create_eyes_ears()
-        (right_hip, left_hip) = create_rlHips()
-        
-        data_shape = new_smpl_np.shape
-        
-        openpose_data_shape = (18, data_shape[1])
-        
-        openpose_data_edited = np.empty(openpose_data_shape, dtype=new_smpl_np.dtype)
-        
-        new_joints = {
-            'Neck': new_neck,
-            'REye': right_eye, 'LEye': left_eye,
-            'REar': right_ear, 'LEar': left_ear,
-            'RHip': right_hip, 'LHip': left_hip
-        }
-        
-        for mdm_joint, openpose_joint in MDM2OPENPOSE_KEYVAL.items():
-            if openpose_joint != REMOVE:
-                openpose_data_edited[OPENPOSE_18JOINT_MAP[openpose_joint],:] = new_smpl_np[MDM_22JOINT_MAP[mdm_joint],:]
-            
-        for new_openpose_joint, data in new_joints.items():
-            openpose_data_edited[OPENPOSE_18JOINT_MAP[new_openpose_joint],:] = data
-            
-                
-        return openpose_data_edited
-
-    new_smpl = convert_mdm2openpose(new_smpl_np)
-
+    
     point_colors = []
     joint_pair_idxs = []
     for color_rgb in OPENPOSE_18JOINT_COLOR.values():
@@ -196,24 +87,25 @@ def main():
     
     renderer = o3d.visualization.rendering.OffscreenRenderer(1024,1024)
     renderer.scene.set_background(np.array([0,0,0,0]))
-    pcd.points = o3d.utility.Vector3dVector(new_smpl)
+    pcd.points = o3d.utility.Vector3dVector(openpose_joints)
     pcd.colors = o3d.utility.Vector3dVector(point_colors)
+    pcd.scale(scale, center=scale_center)
     pcd.scale(1.3, center=scale_center)
     lineSet.points = pcd.points
     lineSet.lines = o3d.utility.Vector2iVector(joint_pair_idxs)
     lineSet.colors = o3d.utility.Vector3dVector(joint_pair_colors)
-    renderer.scene.add_geometry("pcd",pcd,mat_point)
-    renderer.scene.add_geometry("lineset",lineSet,mat_line)
+    renderer.scene.add_geometry("pcd", pcd, mat_point)
+    renderer.scene.add_geometry("lineset", lineSet, mat_line)
     
     camera_position = {'front': [0,0.6+mesh_center[1],1.5], 'back': [0,0.6+mesh_center[1],-1.5], 'left': [1.5,0.6+mesh_center[1],0], 'right': [-1.5,0.6+mesh_center[1],0]}
     
     for angle, value in camera_position.items():
         renderer.setup_camera(60,
-                                mesh_center,
-                                np.array(value),
-                                np.array([0,1,0])
-                                )
+                              mesh_center,
+                              np.array(value),
+                              np.array([0,1,0])
+                              )
         img_o3d = renderer.render_to_image()
-        o3d.io.write_image(os.path.join(args.destinationPath, f"{angle}.png"), img_o3d)
+        o3d.io.write_image(os.path.join(args.destinationPath, f"{TEST_NAME}_{angle}.png"), img_o3d)
     
 main()
